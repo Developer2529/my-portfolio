d1f0a8ddca9036974f44b7dae9f4b322ba6e1543
Richard Sanchez <developer2529@gmail.com>
2024-07-25 14:11:10 -0400

    the navbar url import was modified

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ProfilePage from "../pages/ProfilePage.vue";
-import NavBar from '../pages/NavBar.vue'
+import TheNavBar from  "../pages/TheNavBar.vue";
 // import AboutMe from "../pages/AboutMe.vue";
 // import ExperiencePage from "../pages/ExperiencePage.vue";
 // import MyProjects from "../pages/MyProjects.vue";
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: NavBar,
+      component: TheNavBar,
       redirect: {
         name: "navMenu",
       },
